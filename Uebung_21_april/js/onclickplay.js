a4d189358bce28bd3057e185913c31cf2d8adfd7
chalidbaumann <chalid.baumann@stud.hslu.ch>
2022-04-21 23:20:56 +0200

    test onclick p

--- Uebung_21_april/js/onclickplay.js
+++ Uebung_21_april/js/onclickplay.js
@@ -1,14 +1,31 @@
 
+var toggle = true
 
-function playAudio() {
-    let audioel = document.querySelector("audio");
-    audioel.play();
-    document.querySelector('button').classList.toggle('playstate');
+var songmap = {
+    one: {
+    songInfoId: 'fluessig',
+    songInfoText: '20.04.2022 <br>  2.5 Liter Getränke konsumiert <br> Total 83 Schlücke <br>'
+},
 
+two: {
+    songInfoId: 'two',
+    songInfoText: '8 Schlücke'
+},
+    three: {
+    songInfoId: 'three',
+    songInfoText: '7 Schlücke'
+},
 
 }
 
+function showMessage(songInfo){
 
+if (toggle){
+    document.getElementById(songInfo.songInfoId).innerHTML=songInfo.songInfoText
+}
+else{
+    document.getElementById(songInfo.songInfoId).innerHTML=''
+}
 
-
-
+toggle = !toggle
+}
