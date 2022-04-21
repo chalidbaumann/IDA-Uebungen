6584c7143eb85df9f89ddcd4a138f7a585f1a358
chalidbaumann <chalid.baumann@stud.hslu.ch>
2022-04-21 21:36:26 +0200

    try onclick java

---
+++ Uebung_21_april/js/onclickplay.js
@@ -0,0 +1,14 @@
+
+
+function playAudio() {
+    let audioel = document.querySelector("audio");
+    audioel.play();
+    document.querySelector('button').classList.toggle('playstate');
+
+
+}
+
+
+
+
+
